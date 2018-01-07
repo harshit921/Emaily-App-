31bdec4adfa2b0c0a28ac2f8283713dc814a9542
harsh <harshit_agrawal36@yahoo.com>
2018-01-07 14:55:28 -0800

    Added Logout and cookies

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys.js');
+const mongoose = require('mongoose');
 
+const User = mongoose.model('users');
+
+passport.serializeUser((user,done) =>{
+  done(null,user.id);
+});
+
+passport.deserializeUser((id,done) =>{
+  User.findById(id)
+    .then((user) =>{
+      done(null,user);
+    });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -11,9 +24,19 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken,refreshToken,profile,done) => {
-      console.log(accessToken);
-      console.log('refreshToken' ,refreshToken);
-      console.log('profile', profile);
+      User.findOne({googleId: profile.id})
+          .then((existingUser)=> {
+              if(existingUser) {
+                //record exist.
+                done(null,existingUser);
+              }
+              else{
+                new User({googleId: profile.id}).save()
+                  .then((user) => {
+                    done(null,user);
+                  });
+              }
+          });
     }
   )
 );
