85f1db394d509a7484fc9aefc8718eb2b5dc9bc9
harsh <harshit_agrawal36@yahoo.com>
2018-01-07 23:26:02 -0800

    trying some fix

---
+++ config/devlop.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleclientID: '685604694025-h5h7df3l4mql99fh2m1l3jb2v4p4n5qc.apps.googleusercontent.com',
+  googleClientSecret: 'x_BZbkgzjIx0uX_HNR9bcfTv',
+  mongoURI: 'mongodb://harshit:harsh@ds046267.mlab.com:46267/emaily-dev',
+  cookieKey: 'edhuwefhuiwhfeurihg4ujk29jk3im8fhhh438rjf'
+};
--- config/keys.js
+++ config/keys.js
@@ -1,14 +1,6 @@
-// if(process.env.NODE_env === 'production') {
-//   module.exports = require('./prod');
-// }
-// else {
-//  module.exports =  require('./dev.js');
-// }
-
-
-module.exports = {
-  googleclientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
-};
+if(process.env.NODE_env === 'production') {
+  module.exports = require('./prod');
+}
+else {
+ module.exports =  require('./devlop');
+}
