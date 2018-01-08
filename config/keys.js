e07624735eb7c558af2948acb6c39011383cd32e
harsh <harshit_agrawal36@yahoo.com>
2018-01-07 23:22:28 -0800

    trying some fix

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,14 @@
-if(process.env.NODE_env === 'production') {
-  module.exports = require('./prod');
-}
-else {
- module.exports =  require('./dev');
-}
+// if(process.env.NODE_env === 'production') {
+//   module.exports = require('./prod');
+// }
+// else {
+//  module.exports =  require('./dev.js');
+// }
+
+
+module.exports = {
+  googleclientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
