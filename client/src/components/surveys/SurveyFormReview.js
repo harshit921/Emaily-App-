9be16cf6eae90efa9fdab87c08279775c920d472
harsh <harshit_agrawal36@yahoo.com>
2018-01-29 09:34:05 -0800

    COmpleted frontend to add a new Survey

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,9 +2,10 @@ import React from 'react';
 import {connect } from 'react-redux';
 import formFields from './formFields';
 import _ from 'lodash';
+import {withRouter} from 'react-router-dom';
 import * as actions from '../../actions/index';
 
-const SurveyFormReview = ({onCancel, formValues, submitSurvey}) => {
+const SurveyFormReview = ({onCancel, formValues, submitSurvey, history}) => {
   const reviewFields = _.map(formFields, ({name, label}) => {
     return(
       <div key={name}>
@@ -21,7 +22,7 @@ const SurveyFormReview = ({onCancel, formValues, submitSurvey}) => {
       <h5>Confirm your entries </h5>
       {reviewFields}
       <button className="yellow white-text darken-3 btn-flat" onClick={onCancel}>Back</button>
-      <button className="green btn-flat white-text right" onClick={() => submitSurvey(formValues)}>
+      <button className="green btn-flat white-text right" onClick={() => submitSurvey(formValues, history)}>
         Send Survey
       </button>
     </div>
@@ -32,4 +33,4 @@ function mapStateToProps(state) {
   return {formValues: state.form.surveyForm.values}
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
